fcafda448ef8c2b8bfb64289297d3757d3dfea5d
Jonathan Fernandes <xjhofernandes@gmail.com>
2020-05-31 14:26:44 -0300

    Creating a Monster View Model
    Criando um view model para os montros. E fazendo alguns crimes no código.

--- RagnarokAPI/Service/ItemService.cs
+++ RagnarokAPI/Service/ItemService.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using MongoDB.Driver;
 using RagnarokAPI.Models;
 using RagnarokAPI.Models.Items;
+using RagnarokAPI.ViewModel;
 
 namespace RagnarokAPI.Service
 {
@@ -25,6 +27,35 @@ namespace RagnarokAPI.Service
 
         public Item Get(int id) =>
             _items.Find<Item>(item => item.IdItem.ItemIdentification == id).FirstOrDefault();
+
+        public List<ItemViewModel> Get(List<int> itemList)
+        {
+            var ListItem = new List<ItemViewModel>();
+
+            foreach (var item in itemList)
+            {
+                ListItem.Add(CreateItemViewModel(_items.Find<Item>(x => x.IdItem.ItemIdentification == item).FirstOrDefault()));
+            }
+
+            return ListItem;
+        }
+
+        public ItemViewModel CreateItemViewModel(Item item)
+        {
+            var rx = new Regex("<[^>]*>");
+            item.Description.ItemDesc = item.Description.ItemDesc != null ? rx.Replace(item.Description.ItemDesc, "") : null;
+
+            var itemView = new ItemViewModel()
+            {
+                ItemId = item.IdItem.ItemIdentification,
+                NameItem = item.Name.NameText,
+                CardImgUrl = item.Image.CardImgUrl,
+                CollectionImgUrl = item.Image.CollectionImgUrl,
+                ItemImgUrl = item.Image.itemImgUrl,
+                ItemDescription = item.Description.ItemDesc,
+            };
+            return itemView;
+        }
 
         public void Update(int id, Item itemIn) =>
             _items.ReplaceOne(item => item.IdItem.ItemIdentification == id, itemIn);
