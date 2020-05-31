bd3a31537289ea0b3c3c57897705a6328a50d36f
Jonathan Fernandes <xjhofernandes@gmail.com>
2020-05-31 18:32:42 -0300

    Addings Service
    Adicionando fix do MonsterService e ItensService

--- RagnarokAPI/Service/ItemsService.cs
+++ RagnarokAPI/Service/ItemsService.cs
@@ -1,11 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using MongoDB.Driver;
+using RagnarokAPI.Models;
+using RagnarokAPI.ViewModel;
+using RagnarokAPI.DataBaseSettings;
+using IItemDatabaseSettings = RagnarokAPI.DataBaseSettings.IItemDatabaseSettings;
 
 namespace RagnarokAPI.Service
 {
     public class ItemsService
     {
+        private readonly IMongoCollection<ItemsCollection> _items;
+
+        public ItemsService(IItemDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _items = database.GetCollection<ItemsCollection>(settings.ItemsFixCollectionName);
+        }
+
+        public List<ItemsCollection> Get() =>
+            _items.Find(item => true).ToList();
+
+        public ItemsCollection Get(int id) =>
+            _items.Find<ItemsCollection>(item => item.ItemId == id).FirstOrDefault();
+
+        public void Update(int id, ItemsCollection itemIn) =>
+            _items.ReplaceOne(item => item.ItemId == id, itemIn);
+
+        public void Remove(ItemsCollection itemIn) =>
+            _items.DeleteOne(item => item.Id == itemIn.Id);
+
+        public void Remove(string id) =>
+            _items.DeleteOne(item => item.Id == id);
     }
 }
--- RagnarokAPI/Service/MonstersService.cs
+++ RagnarokAPI/Service/MonstersService.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Driver;
+using RagnarokAPI.Models;
+using RagnarokAPI.DataBaseSettings;
+using IMonsterDatabaseSettings = RagnarokAPI.DataBaseSettings.IMonsterDatabaseSettings;
 
 namespace RagnarokAPI.Service
 {
     public class MonstersService
     {
+        private readonly IMongoCollection<MonstersCollection> _monsters;
+
+        public MonstersService(IMonsterDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _monsters = database.GetCollection<MonstersCollection>(settings.MonstersFixCollectionName);
+        }
+
+        public List<MonstersCollection> Get() =>
+            _monsters.Find(monster => true).ToList();
+
+        public MonstersCollection Get(string id) =>
+            _monsters.Find<MonstersCollection>(monster => monster.MonsterId == id).FirstOrDefault();
+
+        public void Update(string id, MonstersCollection monsterIn) =>
+            _monsters.ReplaceOne(monster => monster.MonsterId == id, monsterIn);
+
+        public void Remove(MonstersCollection monsterIn) =>
+            _monsters.DeleteOne(monster => monster.MonsterId == monsterIn.Id);
+
+        public void Remove(string id) =>
+            _monsters.DeleteOne(monster => monster.MonsterId == id);
     }
 }
