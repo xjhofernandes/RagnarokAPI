2c6fb39eca58eee7bfc990b5787d6016f5be32d8
Jonathan Fernandes <xjhofernandes@gmail.com>
2020-06-02 21:06:14 -0300

    Adding a homepage hello
    Configurando a conexão do mongoDB Atlas com o heroku.

---
+++ RagnarokAPI/Controllers/InicioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RagnarokAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class InicioController : ControllerBase
+    {
+        [HttpGet]
+        public ContentResult Get()
+        {
+            return base.Content("<h1>Hello, Ragnarok API!</h1>", "text/html");
+        }
+    }
+}
--- RagnarokAPI/deploy.bat
+++ RagnarokAPI/deploy.bat
@@ -1,9 +1,9 @@
 docker build --rm -f "Dockerfile" -t "ragnarokapi:latest" .
 
-heroku container:login
+#heroku container:login
 
 
-docker images
+#docker images
 
 heroku container:push web -a ragnarokapi
 
